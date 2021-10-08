d86e8d1c498bf22b795e29c8e12bb824801c7c3d
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-10-09 07:17:11 +0900

    fixed syntax error

--- src/clj/r99c/handler.clj
+++ src/clj/r99c/handler.clj
@@ -20,19 +20,19 @@
   :start
   (ring/ring-handler
    (ring/router
-    [(login-routes) (home-routes) (admin-routes)]))
-  (ring/routes
-   (ring/create-resource-handler
-    {:path "/"})
-   (wrap-content-type
-    (wrap-webjars (constantly nil)))
-   (ring/create-default-handler
-    {:not-found
-     (constantly (error-page {:status 404, :title "404 - Page not found"}))
-     :method-not-allowed
-     (constantly (error-page {:status 405, :title "405 - Not allowed"}))
-     :not-acceptable
-     (constantly (error-page {:status 406, :title "406 - Not acceptable"}))})))
+    [(login-routes) (home-routes) (admin-routes)])
+   (ring/routes
+    (ring/create-resource-handler
+     {:path "/"})
+    (wrap-content-type
+     (wrap-webjars (constantly nil)))
+    (ring/create-default-handler
+     {:not-found
+      (constantly (error-page {:status 404, :title "404 - Page not found"}))
+      :method-not-allowed
+      (constantly (error-page {:status 405, :title "405 - Not allowed"}))
+      :not-acceptable
+      (constantly (error-page {:status 406, :title "406 - Not acceptable"}))}))))
 
 (defn app []
   (middleware/wrap-base #'app-routes))
