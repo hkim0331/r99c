d3f447f98a1e8a041b1d0603a7dc133c712bb833
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-11-07 10:04:31 +0900

    u.c.

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -2,14 +2,14 @@
   "Userspace functions you can run by default in your local REPL."
   (:require
    [r99c.config :refer [env]]
-    [clojure.pprint]
-    [clojure.spec.alpha :as s]
-    [expound.alpha :as expound]
-    [mount.core :as mount]
-    [r99c.core :refer [start-app]]
-    [r99c.db.core]
-    [conman.core :as conman]
-    [luminus-migrations.core :as migrations]))
+   [clojure.pprint]
+   [clojure.spec.alpha :as s]
+   [expound.alpha :as expound]
+   [mount.core :as mount]
+   [r99c.core :refer [start-app]]
+   [r99c.db.core]
+   [conman.core :as conman]
+   [luminus-migrations.core :as migrations]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
