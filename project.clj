c0733f89de7d98c1cee12875ddb7f57194bf4116
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-11-21 23:48:36 +0900

    0.8.22 fake no-check button

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject r99c "0.8.21"
+(defproject r99c "0.8.22"
 
   :description "r99 revised with clojure"
   :url "https://r99.melt.kyutech.ac.jp"
--- src/clj/r99c/routes/login.clj
+++ src/clj/r99c/routes/login.clj
@@ -9,7 +9,7 @@
    [struct.core :as st]
    [taoensso.timbre :as timbre]))
 
-(def ^:private version "0.8.21")
+(def ^:private version "0.8.22")
 
 (def users-schema
   [[:sid
