37cc47ad062e132d06c9e6dbd1daa2358dbad421
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-01-16 15:01:06 +0900

    hotfix 0.17.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject r99c "0.17.0"
+(defproject r99c "0.17.1"
 
   :description "r99 revised with clojure"
   :url "https://r99.melt.kyutech.ac.jp"
--- src/clj/r99c/routes/login.clj
+++ src/clj/r99c/routes/login.clj
@@ -9,7 +9,7 @@
    [struct.core :as st]
    [taoensso.timbre :as timbre]))
 
-(def ^:private version "0.17.0")
+(def ^:private version "0.17.1")
 
 (def users-schema
   [[:sid
