926aad2f05fbfbfc053bb4479a75cc3cc6afbe33
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-02-01 09:16:43 +0900

    selmer upgraded to 1.12.50

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
    [ring-webjars "0.2.0"]
    [ring/ring-core "1.9.5"]
    [ring/ring-defaults "0.3.3"]
-   [selmer "1.12.49"]
+   [selmer "1.12.50"]
    ;;
    [buddy/buddy-auth "3.0.323"]
    [buddy/buddy-hashers "1.8.158"]
