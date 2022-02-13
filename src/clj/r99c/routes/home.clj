aa714b8ef15700d20b09a01c748beaf808a3850f
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-02-14 00:16:06 +0900

    add wp.html

--- src/clj/r99c/routes/home.clj
+++ src/clj/r99c/routes/home.clj
@@ -74,7 +74,7 @@
              count)]
    (if (zero? c)
     (str "")
-    (str "(+" c ")"))))
+    (str c "⤵️"))))
 
 (add-filter! :wrap66  (fn [x] (wrap 66 x)))
 (add-filter! :first-line (fn [x] (first-line x)))
